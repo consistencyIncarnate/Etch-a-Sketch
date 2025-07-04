c33214b796162d8eb8173226188fe01ca8801614
Strahinja <1711365+consistencyIncarnate@users.noreply.github.com>
2025-07-04 15:09:23 +0200

    Add grid change background color on hover effect

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const gridContainer = document.getElementById("container");
 
-
 for(let i = 0; i < 16*16; i++){
     const gridDiv = document.createElement("div");
     gridDiv.setAttribute("class", "gridDiv");
     gridContainer.appendChild(gridDiv);
-}
+};
 
+const gridDivGlobal = document.querySelectorAll(".gridDiv");
 
+gridContainer.addEventListener("click", () => {
+    gridDivGlobal.forEach((n) => {
+        n.addEventListener("mouseover", (e) => {
+            e.currentTarget.style.backgroundColor = "black"
+        });
+    });
+});
